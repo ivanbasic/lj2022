8dd76854d02f71ad2ef0e0f606b0aafd5d6d718b
Ivan Basic <ivanbasic@ns>
2022-10-28 22:34:17 +0200

    fake constructor

--- src/ivanbasic/java_11_02_001_CreatingObjects.java
+++ src/ivanbasic/java_11_02_001_CreatingObjects.java
@@ -2,47 +2,52 @@ package ivanbasic;
 
 public class java_11_02_001_CreatingObjects implements Lesson {
 	public void main() {
-		System.out.println("Creating Objects. Order of Initialization");		
-		
+		System.out.println("Creating Objects. Order of Initialization");
+
 		Chick chick= new Chick();
-		
+
 	}
-	
+
 }
 
 class Chick {
+
+	// Constructor
 	public Chick() {
 		cPrivate = "constructor";
 		cStatic = "constructor";
-		
+
 		System.out.println("constructor, always the last, cPrivate="+cPrivate);
 		System.out.println("constructor, always the last, cStatic="+cStatic);
-	}	
-	
-	
+	}
+
+	// Fake constructor. ( It’s a regular method)
+	public void Chick() { }
+
+
 	private String cPrivate = "setting field";
 	private static String cStatic="setting field"; //ignored?
 
-	{System.out.println("1. Block, field cPrivate="+cPrivate);}	
+	{System.out.println("1. Block, field cPrivate="+cPrivate);}
 	{System.out.println("1. Block, field cStatic="+cStatic);}
 
-	
+
 
 	{ cPrivate = "block"; }
-	{ cStatic = "block"; }	
-	{System.out.println("2. Block, field cPrivate="+cPrivate);}	
+	{ cStatic = "block"; }
+	{System.out.println("2. Block, field cPrivate="+cPrivate);}
 	{System.out.println("2. Block, field cStatic="+cStatic);}
 
 
 
- 	
-	static { 
+
+	static {
 		cStatic = "static initializer, always FIRST";
-		
+
 		//executed, before this lesson, and before first lesson!!!
 		System.out.println("Lesson CreatingObjects, ");
 		System.out.println("Inside static Initializer Block, field, cStatic="+cStatic);
-	}	
-	
+	}
+
 }
 
