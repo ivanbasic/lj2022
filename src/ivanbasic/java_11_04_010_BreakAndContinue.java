9abb8b59620121f2486dd7ea30440669c40d7b58
Ivan Basic <ivanbasic@ns>
2022-11-01 13:40:45 +0100

    Break And Continue

---
+++ src/ivanbasic/java_11_04_010_BreakAndContinue.java
@@ -0,0 +1,32 @@
+package ivanbasic;
+
+public class java_11_04_010_BreakAndContinue implements Lesson{
+    public void main() {
+
+        System.out.println("");
+        System.out.print("CONTINUE ON 3:");
+        for( int i=1; i<=10; i++) {
+            if (i==3) {
+                System.out.print(" (skip the rest of current iteration)" );
+                continue;
+            }
+            System.out.print(" "+i);
+        }
+
+        System.out.println("");
+        System.out.print("BREAK ON 3:");
+        for( int i=1; i<=10; i++) {
+            if (i==3) {
+                System.out.println(" (Leave the loop)");
+                break;
+            }
+            System.out.print(" "+i);
+        }
+
+
+
+
+
+    }
+
+}
