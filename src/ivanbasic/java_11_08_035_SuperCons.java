414933bee26fe9c3e0c99f5a6fb113985e2651d2
Ivan Basic <ivanbasic@ns>
2022-11-04 22:24:33 +0100

    super constructor

--- src/ivanbasic/java_11_08_035_SuperCons.java
+++ src/ivanbasic/java_11_08_035_SuperCons.java
@@ -2,23 +2,35 @@ package ivanbasic;
 
 public class java_11_08_035_SuperCons implements Lesson {
 
-	public void main() {
- 
-		Second sec = new Second();
-	}
+    public void main() {
 
-	
+        Second sec = new Second();
+
+        System.out.println("");
+        Second sec2 = new Second(1);
+    }
+
+
 }
+
 class First {
-	First() {
-		System.out.println("first");
-	}
-	
+    First() {
+        System.out.println("first()");
+    }
+
+    First(int i) {
+        System.out.println("first(int)");
+    }
 }
+
 class Second extends First {
-	public Second() {
-		//super();  //commented or not, it's the same.
-		System.out.println("second");		
-	}
-	
+    public Second() {
+        //super();  //commented or not, it's the same.
+        System.out.println("second()");
+    }
+
+    public Second(int i) {
+        System.out.println("second(int) calls super()=first() without arguments! ");
+    }
+
 }
